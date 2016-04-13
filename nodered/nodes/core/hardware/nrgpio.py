dd7a50baae3a841ed2e4359170f3c2e27f13e590
Sharbel Cherian <sharbel@clearfoodinc.com>
2016-04-13 09:49:20 +0530

    Feature C1 - New Commit

--- nodered/nodes/core/hardware/nrgpio.py
+++ nodered/nodes/core/hardware/nrgpio.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright 2014,2016 IBM Corp.
+# Copyright 2014,2016 console.log.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
